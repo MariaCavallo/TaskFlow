62362c6ffd3c5a0e1c33f1f8013df1305448d3c1
Maria Cavallo <mariacavallom@gmail.com>
2023-10-09 11:05:05 -0300

    Fixing some things

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import {Routes, Route, useLocation, useNavigate } from 'react-router-dom'
 import './App.css'
 import { NavBar } from './Components/Common/NavBar/NavBar'
 import Footer from './Components/Common/Footer/Footer'
 import DarkModeToggle from './Components/Utils/DarkMode';
+import Home from './Components/Pages/Home/Home';
+import ToDoPage from './Components/Pages/ToDo\'s/ToDoPage';
+import { Provider } from 'react-redux';
+import store from './Components/redux/store';
 
 function App() {  
   const location = useLocation();
@@ -14,19 +18,25 @@ function App() {
     if (location.pathname === '/') {
       navigate('/home')
     }
-  })
+  }, [location, navigate])
   
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode)
   };
   
   return (
-    <div className={`App ${isDarkMode ? 'dark' : 'light'}`}>
-        <NavBar />
-        <DarkModeToggle isDarkMode={isDarkMode} onToggle={toggleDarkMode}/>
-        <Outlet />
-        <Footer />
-    </div>
+    <Provider store={store}>
+      <div className={`App ${isDarkMode ? 'dark' : 'light'}`}>
+          <NavBar />
+          <DarkModeToggle isDarkMode={isDarkMode} onToggle={toggleDarkMode}/>
+          <Routes>
+            <Route index path="/home" element={<Home/>}/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/todosPage" element={<ToDoPage/>}/>
+          </Routes>
+          <Footer />
+      </div>
+    </Provider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import Home from './Components/Pages/Home/Home.tsx'
-import { Provider } from 'react-redux'
-import store from './Components/redux/store.ts'
-import ToDoPage from './Components/Pages/ToDo\'s/ToDoPage.tsx'
 
 ReactDOM.render(
     <React.StrictMode>
-      <Provider store={store}>
-          <BrowserRouter>
-              <Routes>
-                <Route element={<App/>}>
-                  <Route index path='/home' element={<Home/>}/>
-                  <Route path='/' element={<Home/>}/>
-                  <Route path='/todosPage' element={<ToDoPage />}/>
-                </Route>
-              </Routes>
-          </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
     </React.StrictMode>,
   document.getElementById('root')
 )
